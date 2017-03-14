5afe282be7b3efc726f54a80ef53fa343ea0d327
Suver1 <andreashellesvik@hotmail.com>
2017-03-14 20:37:12 +0100

    Create WebView and open /goto links in default browser

--- BrowserTest/BrowserTestPage.xaml.cs
+++ BrowserTest/BrowserTestPage.xaml.cs
@@ -1,12 +1,41 @@
 ﻿using Xamarin.Forms;
+using System;
 
 namespace BrowserTest
 {
 	public partial class BrowserTestPage : ContentPage
 	{
+		WebView webView;
+
 		public BrowserTestPage()
 		{
 			InitializeComponent();
+
+			webView = new WebView();
+			webView.Source = "https://www.prisguide.no";
+
+			// Accomodate iPhone status bar.
+			Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+			webView.Navigating += (sender, ev) =>
+			{
+				if (ev.Url.Contains("prisguide.no/goto.php"))
+				{
+					try
+					{
+						var uri = new Uri(ev.Url);
+						Device.OpenUri(uri);
+					}
+					catch (Exception e)
+					{
+						System.Diagnostics.Debug.WriteLine("Exception: " + e);
+					}
+
+					ev.Cancel = true;
+				}
+			};
+
+			Content = webView;
 		}
 	}
 }
